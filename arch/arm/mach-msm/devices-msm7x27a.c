1fbe73648981cf654aac7370f8d5dffc0c991cc5
KlinkOnE <klinkone@gmail.com>
2014-06-04 13:06:13 +0200

    restore gpu to 300MHz

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -679,24 +679,20 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 333000000,
-			.bus_freq = 220000000,
+			.gpu_freq = 300000000,
+			.bus_freq = 2,
 		},
 		{
-			.gpu_freq = 245760000,
-			.bus_freq = 200000000,
+			.gpu_freq = 200000000,
+			.bus_freq = 1,
 		},
 		{
-			.gpu_freq = 192000000,
-			.bus_freq = 160000000,
-		},
-		{
-			.gpu_freq = 133330000,
+			.gpu_freq = 100000000,
 			.bus_freq = 0,
 		},
 	},
 	.init_level = 0,
-	.num_levels = 4,
+	.num_levels = 3,
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
 	.strtstp_sleepwake = true,
